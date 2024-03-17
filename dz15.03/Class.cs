d1c71cfcaae805d0032141dda3495d55574bfb67
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-17 12:44:47 +0200

    done

--- dz15.03/Class.cs
+++ dz15.03/Class.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+
+class LandTransport : ITransport
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Товар доставлено на суші.");
+    }
+}
+
+class AirTransport : ITransport
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Товар доставлено повітрям.");
+    }
+}
+
+class SeaTransport : ITransport
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Товар доставлено морем.");
+    }
+}
+
+class TransportFactory
+{
+    public static ITransport CreateTransport(string transportType)
+    {
+        switch (transportType.ToLower())
+        {
+            case "land":
+                return new LandTransport();
+            case "air":
+                return new AirTransport();
+            case "sea":
+                return new SeaTransport();
+            default:
+                throw new ArgumentException("Невідомий тип транспорту");
+        }
+    }
+}
--- dz15.03/Program.cs
+++ dz15.03/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        ITransport landTransport = TransportFactory.CreateTransport("land");
+        landTransport.Deliver();
+
+        ITransport airTransport = TransportFactory.CreateTransport("air");
+        airTransport.Deliver();
+
+        ITransport seaTransport = TransportFactory.CreateTransport("sea");
+        seaTransport.Deliver();
+    }
+}
