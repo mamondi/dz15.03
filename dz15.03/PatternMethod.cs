f77c57d2761112d2d76cc8d0d07565ac55f6cd22
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-17 13:40:48 +0200

    done

--- dz15.03/PatternMethod.cs
+++ dz15.03/PatternMethod.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+public sealed class Singleton
+{
+    private static Singleton instance;
+    private static readonly object lockObject = new object();
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                    Console.WriteLine("Створено новий екземпляр Singleton.");
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
--- dz15.03/Program.cs
+++ dz15.03/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Singleton instance1 = Singleton.Instance;
+        instance1.PrintMessage("Повідомлення з першого екземпляра");
+
+        Singleton instance2 = Singleton.Instance;
+        instance2.PrintMessage("Повідомлення з другого екземпляра");
+    }
+}
